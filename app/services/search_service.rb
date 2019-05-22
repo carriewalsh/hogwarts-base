595fc6c9715b5250ee86105f87850bb1ffd34043
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-22 15:35:03 -0600

    Privatizes methods in SearchService

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -4,21 +4,6 @@ class SearchService
     @house = house
   end
 
-  def conn(params)
-    Faraday.new("https://hogwarts-as-a-service.herokuapp.com/api/v1/house/#{params}") do |f|
-      f.headers["x_api_key"] = ENV["HOGWARTS_SERVICE_KEY_OF_MYSTERIES"]
-      f.adapter Faraday.default_adapter
-    end
-  end
-
-  def accio_houses
-    conn("").get.body
-  end
-
-  def accio_students(house)
-    conn("/#{accio_id(house)}").get.body
-  end
-
   def jsonificus_students
     JSON.parse(accio_students(@house))
   end
@@ -28,11 +13,24 @@ class SearchService
   end
 
   private
+  
+    def conn(params)
+      Faraday.new("https://hogwarts-as-a-service.herokuapp.com/api/v1/house/#{params}") do |f|
+        f.headers["x_api_key"] = ENV["HOGWARTS_SERVICE_KEY_OF_MYSTERIES"]
+        f.adapter Faraday.default_adapter
+      end
+    end
+
+    def accio_houses
+      conn("").get.body
+    end
+
+    def accio_students(house)
+      conn("/#{accio_id(house)}").get.body
+    end
 
     def accio_id(param)
-      houses = jsonificus_house
-      @_id = nil
-      houses.each do |house|
+      jsonificus_house.each do |house|
         if house["name"] == param
           @_id = house["id"]
         end
