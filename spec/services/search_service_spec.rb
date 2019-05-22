7fe6efcca69b9b5f3b30e3d6f75749840a6656d4
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-22 14:05:33 -0600

    Adds service spec

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -3,8 +3,14 @@ require "rails_helper"
 
 describe SearchService do
   it "can retrieve student data from Hotwarts api" do
-    body = Faraday.get("http://hogwarts-it.herokuapp.com/api/v1/house/slytherin?api_key=#{ENV["HOGWARTS_KEY_OF_MYSTERIES"]}")
-    data = JSON.parse(body)
-    expect()
+    response = Faraday.get("http://hogwarts-it.herokuapp.com/api/v1/house/slytherin?api_key=#{ENV['HOGWARTS_KEY_OF_MYSTERIES']}")
+
+    results = JSON.parse(response.body)["data"].first
+
+    expect(results["attributes"]["name"]).to eq("Slytherin")
+    expect(results["attributes"]["students"]).to be_an(Array)
+    expect(results["attributes"]["students"].count).to eq(22)
+    expect(results["attributes"]["students"].first["name"]).to eq("Gormlaith Gaunt")
+    expect(results["attributes"]["students"].first["id"]).to eq(52)
   end
 end
