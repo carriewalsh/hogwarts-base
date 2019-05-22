4f4a96870a0c68f65d852e5988e8ab5dd6db866f
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-22 15:10:50 -0600

    Changes spec and adds new methods for round_two

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,14 +1,30 @@
 class SearchService
 
-  def initialize(house)
-    @house = house
+  def initialize(id)
+    @id = id
   end
 
-  def conn(house)
-    Faraday.get("http://hogwarts-it.herokuapp.com/api/v1/house/#{house}?api_key=#{ENV['HOGWARTS_KEY_OF_MYSTERIES']}")
+  def conn(params)
+    Faraday.new("https://hogwarts-as-a-service.herokuapp.com/api/v1/house/#{params}") do |f|
+      f.headers["x_api_key"] = ENV["HOGWARTS_SERVICE_KEY_OF_MYSTERIES"]
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def accio_houses
+    conn("").get
+  end
+
+  def accio_students(id)
+    conn("/#{id}").get
   end
 
   def jsonificus
-    JSON.parse(conn(@house).body)["data"].first["attributes"]
+    if @id.nil?
+      JSON.parse(accio_houses.body)
+    else
+      binding.pry
+      JSON.parse(accio_students(@id).body)
+    end
   end
 end
--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -2,37 +2,17 @@ require "rails_helper"
 
 
 describe SearchService do
-  it "can retrieve student data from Hotwarts api" do
-    results = SearchService.new("slytherin").jsonificus
-
-    expect(results["name"]).to eq("Slytherin")
-    expect(results["students"]).to be_an(Array)
-    expect(results["students"].count).to eq(22)
-    expect(results["students"].first["name"]).to eq("Gormlaith Gaunt")
-    expect(results["students"].first["id"]).to eq(52)
-  end
-
-  xit "can retrieve house data from hogwarts service api" do
-    response = Faraday.new("https://hogwarts-as-a-service.herokuapp.com/api/v1/house") do |f|
-      f.headers["x_api_key"] = ENV["HOGWARTS_SERVICE_KEY_OF_MYSTERIES"]
-      f.adapter Faraday.default_adapter
-    end
-    data = response.get
-    results = JSON.parse(data.body)
+  it "can retrieve house data from hogwarts service api" do
+    results = SearchService.new("").jsonificus
 
     expect(results).to be_an(Array)
     expect(results.first["name"]).to eq("Gryffindor")
   end
 
-  xit "can retrieve house data from hogwarts service api" do
-    response = Faraday.new("https://hogwarts-as-a-service.herokuapp.com/api/v1/house") do |f|
-      f.headers["x_api_key"] = ENV["HOGWARTS_SERVICE_KEY_OF_MYSTERIES"]
-      f.adapter Faraday.default_adapter
-    end
-    data = response.get
-    results = JSON.parse(data.body)
+  it "can retrieve house data from hogwarts service api" do
+    results = SearchService.new(1).jsonificus
 
     expect(results).to be_an(Array)
-    expect(results.first["name"]).to eq("Gryffindor")
+    expect(results.first["name"]).to eq("Albus Dumbledore")
   end
 end
