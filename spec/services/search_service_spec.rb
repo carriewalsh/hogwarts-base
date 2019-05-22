a0b971bea0a55970dc3469784e914f109b6851bf
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-22 14:43:20 -0600

    Adds new specs for new api

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -3,14 +3,36 @@ require "rails_helper"
 
 describe SearchService do
   it "can retrieve student data from Hotwarts api" do
-    response = Faraday.get("http://hogwarts-it.herokuapp.com/api/v1/house/slytherin?api_key=#{ENV['HOGWARTS_KEY_OF_MYSTERIES']}")
+    results = SearchService.new("slytherin").jsonificus
 
-    results = JSON.parse(response.body)["data"].first
+    expect(results["name"]).to eq("Slytherin")
+    expect(results["students"]).to be_an(Array)
+    expect(results["students"].count).to eq(22)
+    expect(results["students"].first["name"]).to eq("Gormlaith Gaunt")
+    expect(results["students"].first["id"]).to eq(52)
+  end
 
-    expect(results["attributes"]["name"]).to eq("Slytherin")
-    expect(results["attributes"]["students"]).to be_an(Array)
-    expect(results["attributes"]["students"].count).to eq(22)
-    expect(results["attributes"]["students"].first["name"]).to eq("Gormlaith Gaunt")
-    expect(results["attributes"]["students"].first["id"]).to eq(52)
+  xit "can retrieve house data from hogwarts service api" do
+    response = Faraday.new("https://hogwarts-as-a-service.herokuapp.com/api/v1/house") do |f|
+      f.headers["x_api_key"] = ENV["HOGWARTS_SERVICE_KEY_OF_MYSTERIES"]
+      f.adapter Faraday.default_adapter
+    end
+    data = response.get
+    results = JSON.parse(data.body)
+
+    expect(results).to be_an(Array)
+    expect(results.first["name"]).to eq("Gryffindor")
+  end
+
+  xit "can retrieve house data from hogwarts service api" do
+    response = Faraday.new("https://hogwarts-as-a-service.herokuapp.com/api/v1/house") do |f|
+      f.headers["x_api_key"] = ENV["HOGWARTS_SERVICE_KEY_OF_MYSTERIES"]
+      f.adapter Faraday.default_adapter
+    end
+    data = response.get
+    results = JSON.parse(data.body)
+
+    expect(results).to be_an(Array)
+    expect(results.first["name"]).to eq("Gryffindor")
   end
 end
