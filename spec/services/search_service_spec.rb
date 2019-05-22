24c8d1075291a5f4dda7b482796f2de63bdc5be3
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-22 14:44:26 -0600

    Fixes the silliness in the ServiceSpec

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -3,14 +3,12 @@ require "rails_helper"
 
 describe SearchService do
   it "can retrieve student data from Hotwarts api" do
-    response = Faraday.get("http://hogwarts-it.herokuapp.com/api/v1/house/slytherin?api_key=#{ENV['HOGWARTS_KEY_OF_MYSTERIES']}")
+    results = SearchService.new("slytherin").jsonificus
 
-    results = JSON.parse(response.body)["data"].first
-
-    expect(results["attributes"]["name"]).to eq("Slytherin")
-    expect(results["attributes"]["students"]).to be_an(Array)
-    expect(results["attributes"]["students"].count).to eq(22)
-    expect(results["attributes"]["students"].first["name"]).to eq("Gormlaith Gaunt")
-    expect(results["attributes"]["students"].first["id"]).to eq(52)
+    expect(results["name"]).to eq("Slytherin")
+    expect(results["students"]).to be_an(Array)
+    expect(results["students"].count).to eq(22)
+    expect(results["students"].first["name"]).to eq("Gormlaith Gaunt")
+    expect(results["students"].first["id"]).to eq(52)
   end
 end
